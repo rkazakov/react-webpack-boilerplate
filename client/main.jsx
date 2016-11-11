dfadd0df2cd294db4a4c0d840d6f344f30622d48
Ruslan Kazakov <Ruslan.Kazakov@myob.com>
2016-11-12 00:07:26 +1100

    Adding redux and todo sample app

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,25 @@
 'use strict';
 
-import 'styles/main.scss';
+import 'styles/main.scss'
 
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 
-import Index from 'components/Index/Index';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-render(<Index items={[1,2,3]} />, document.getElementById('js-main'));
+import todoApp from './reducers'
+import App from './components/App'
+
+//import Index from 'components/Index/Index';
+
+//let store = createStore(todoApp)
+const store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('js-main')
+);
